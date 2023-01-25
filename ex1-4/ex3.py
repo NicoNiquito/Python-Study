5929920d5c0d84f8a3f4afa93f9214d7cdebb39c
Nicolas <nicolas_rodrigues_27@outlook.com>
2023-01-25 01:44:28 -0300

    ex3
    lê 2 números e faz a soma entre eles

---
+++ ex1-4/ex3.py
@@ -0,0 +1,7 @@
+#-> Desafio 3
+#   ->Crie um script Python que leia dois números e mostre a soma entre eles
+
+num1 = int(input('Diga um número'))
+num2 = int(input('Diga outro número'))
+s = num1 + num2
+print('A soma de {} e {} resulta em {}'.format(num1, num2, s))
