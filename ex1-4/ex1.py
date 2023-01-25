87288fa0a10086aa881ca64993d686918b313d8d
Nicolas <nicolas_rodrigues_27@outlook.com>
2023-01-25 01:42:34 -0300

    ex1
    lê o nome e manda uma mensagem

---
+++ ex1-4/ex1.py
@@ -0,0 +1,5 @@
+#-> Desafio 1
+#   ->Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de acordo com o valor digitado
+
+name = input('Whats your name?')
+print('Hello {}, how are you doing?'.format(name))
