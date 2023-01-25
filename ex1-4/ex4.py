2f8ec42ea30b387a909e53e21d55a8c53da7c320
Nicolas <nicolas_rodrigues_27@outlook.com>
2023-01-25 01:45:07 -0300

    ex4
    lê o que for enviado e dá algumas informações sobre

---
+++ ex1-4/ex4.py
@@ -0,0 +1,11 @@
+#-> Desafio 4
+#   ->Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+
+x = input('Digite algo')
+print('O tipo primitivo é:', type(x))
+print('É formado por letra(s)?', x.isalpha())
+print('É formado por número(s)?', x.isnumeric())
+print('É formado por letras e números?', x.isalnum())
+print('Está em maiúsculo?', x.isupper())
+print('Está em minúsculo?', x.islower())
+print('Está capitalizado?', x.istitle())
