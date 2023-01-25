8162aa3e6388cc9f73e1254763aecbdd673a7734
Nicolas <nicolas_rodrigues_27@outlook.com>
2023-01-25 01:43:56 -0300

    ex2
    lê data, mês e ano e mostra uma mensagem

---
+++ ex1-4/ex2.py
@@ -0,0 +1,7 @@
+#-> Desafio 2
+#   ->Crie um script Python que leia a data, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada
+
+data = input('Em que dia você nasceu?')
+mes = input('Em qual mês você nasceu?')
+ano = input('Em que ano você nasceu?')
+print('Você nasceu dia {} de {} de {}.'.format(data, mes, ano))
