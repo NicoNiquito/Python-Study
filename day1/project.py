73bbd92e55260b33911f1eef053b3329e76ab3c8
Nicolas <nicolas_rodrigues_27@outlook.com>
2023-01-25 21:32:42 -0300

    day 1 project

---
+++ day1/project.py
@@ -0,0 +1,6 @@
+# Band Name Generator
+
+print('Welcome to the Band Name Generator.')
+city = input("Which city did you grow up in?\n")
+pet = input("What's your pet's name?\n")
+print('your band name could be {} {}'.format(city, pet))
