66e7b50453633a152268d4b3b2f9030e5b657bb7
Nicolas <nicolas_rodrigues_27@outlook.com>
2023-01-25 21:12:13 -0300

    day 1 ex 4

---
+++ day1/ex4.py
@@ -0,0 +1,12 @@
+# To solve this one, I had to switch the variable values, without changing the lines 3-4 and 10-11
+
+a = input("a: ")
+b = input("b: ")
+#
+c = a
+a = b
+b = c
+#
+print("a: " + a)
+print("b: " + b)
+  
