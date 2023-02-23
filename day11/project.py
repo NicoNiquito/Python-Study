9e91b027cb2dbf5c38cbe8a58d9a80210b86b1c3
Nicolas <nicolas_rodrigues_27@outlook.com>
2023-02-22 23:49:48 -0300

    project

--- day11/project.py
+++ day11/project.py
@@ -15,17 +15,4 @@ import random
 import art
 
 
-
-def deal_card():
-    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    card = random.choice(cards)
-    return card
-
-user_cards = []
-computer_cards = []
-
-for card in range(2):
-    new_card = deal_card()
-    user_cards.append(new_card)
-
-# To be continued
+    
